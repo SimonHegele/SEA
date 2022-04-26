f1245f13e0c77a8655063fcd695864e77e46f3a1
SimonHegele <103111576+SimonHegele@users.noreply.github.com>
2022-04-26 23:04:11 +0200

    Add files via upload

---
+++ Read_RNDWs.py
@@ -0,0 +1,21 @@
+import json
+import os
+
+def file_list(path):
+    """
+    In: path to a folder
+    Out: list of elements in the folder
+    """
+    mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
+    return list(sorted(os.listdir(path), key=mtime))
+
+def read_RNDWs_from_JSONs(path):
+    """
+    In: path to folder
+    Out: List of Dictionairies from JSON-files in the folder
+    """
+    rndws = []
+    for filename in file_list(path):
+        with open(filename, 'r') as f:
+            rndw = json.loads(f.read())
+        rndws.append(rndw)
